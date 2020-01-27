90140b0f432c03598bb37bd97c80ad9b2795971d
andelf <andelf@gmail.com>
2020-01-27 20:24:12 +0800

    pbc: refactor pbc example

---
+++ pbc/examples/tutorial.rs
@@ -0,0 +1,34 @@
+use pbc::{Element, Pairing};
+use std::fs;
+
+pub fn main() {
+    let param = fs::read_to_string("param/a.param").unwrap();
+    let pairing: Pairing = param.parse().unwrap();
+    let mut g = Element::init_g2(&pairing);
+    let mut h = Element::init_g1(&pairing);
+
+    let mut temp1 = Element::init_gt(&pairing);
+    let mut temp2 = Element::init_gt(&pairing);
+
+    let mut secret_key = Element::init_zr(&pairing);
+
+    // generate system parameters
+    g.random();
+
+    // generate a private key
+    secret_key.random();
+
+    let public_key = Element::pow_zn(&g, &secret_key);
+    h.from_hash(b"ABCDEF");
+
+    let sig = Element::pow_zn(&h, &secret_key);
+
+    pairing.apply(&mut temp1, &sig, &g);
+    pairing.apply(&mut temp2, &h, &public_key);
+
+    if temp1 == temp2 {
+        println!("signature verifies");
+    } else {
+        println!("signature does not verify");
+    }
+}
